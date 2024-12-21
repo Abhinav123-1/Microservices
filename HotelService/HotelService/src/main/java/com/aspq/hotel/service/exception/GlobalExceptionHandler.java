a0c72ecaae20bcd8c3b5facf6f4f8cb79805245a
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-12-21 15:22:44 +0530

    Hotel Service Code

--- HotelService/HotelService/src/main/java/com/aspq/hotel/service/exception/GlobalExceptionHandler.java
+++ HotelService/HotelService/src/main/java/com/aspq/hotel/service/exception/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.aspq.hotel.service.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 //we will writee code about Exception hANDLING...
-    //public ResponseEntity<APIResponse>
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String,Object>> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException) {
+        return null;
+    }
 
 }
